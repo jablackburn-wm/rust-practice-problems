8304d74d02bfecea5959317c3a7e43b3ad32e484
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-10-18 19:52:35 -0400

    Create comment skeleton

---
+++ median_and_mode/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    // initialize vector
+
+    //let mut v = vec![1, 2, 3, 4, 5, 6]
+    //let mut v = vec![2, -100, 1000, 21, 123, 2, 2, 3, 3, 3, 4, 5]
+    //let mut v = vec![1, 1, 1, 1, 1];
+    let mut v = vec![1, 5, 2, 70];
+
+    // median
+        // sort
+        // get midpoint(s)
+        // calculate median
+        // print median
+    // mode
+        // init hashmap
+        // iterate over vector like in listing 8-25 (official rust book)
+        // create modes vec
+        // iterate over value pairs in hashmap, store max value(s) in the vec
+        // print modes
+}
